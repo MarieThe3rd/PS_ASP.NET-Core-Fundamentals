e9a474716ec411412d42b6704e5a0c58ce78f66f
Marie Robinson <mariethe3rd@gmail,com>
2019-12-16 13:04:18 -0600

    add hello from app settings

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
         public void OnGet()
         {
-
+            Message = config["Message"];
         }
     }
 }
